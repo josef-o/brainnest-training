dd4bc4c172c5fce47294feb8bb1317b73649f6c9
josef <josheshin@gmail.com>
2023-04-12 09:58:05 -0300

    Adds case and multiple letter tolerance to hangman.py

--- assignment-1/hangman.py
+++ assignment-1/hangman.py
@@ -21,43 +21,55 @@ Word: j a _ a
 Guess a letter: v                                                                                                                                                
 You guessed the word java !
 '''
-from random import randint
+import random
 
-WORDS = ["tree", "car", "lordoftherings", "godzilla", "disestablishmentarianism", "kapow", "java", "foobar"]
+WORDS = ["tree", "car", "queue", "equilibrium", "godzilla", "disestablishmentarianism", "kapow", "java", "foobar"]
 
-def get_indices(letter: str, word: str):
+def get_indices(letter: str, word: str) -> list[int]:
+    """
+    Returns the indices in word that match the given letter.
+    """
     indices = []
     for i in range(len(word)):
         if word[i] == letter:
             indices.append(i)
     return indices
 
-
-word = WORDS[randint(0, len(WORDS))]
-
+# pick a word to guess from a collection of predefined ones
+word = random.choice(WORDS)
 guess = ["_"] * len(word)
-
 tries_left = 6
 used_letters = set()
-    
+# to seed the input loop
+user_letter = ""
+
 while tries_left > 0:
-    print("You have", tries_left, "tries left.")
+    print(f"You have {tries_left} tries left.")
     print("Used letters:", " ".join(used_letters))
     print("Word:", " ".join(guess))
     print()
     
-    user_letter = input("Guess a letter: ")
+    while len(user_letter) != 1:
+        print("Please enter a single letter: ", end="")
+        user_letter = input().lower()
+        # reject already guessed letters
+        if user_letter in used_letters:
+            print("You have already chosen that letter, pick a different one.")
+            user_letter = ""
+            
     used_letters.add(user_letter)
-    guessed = get_indices(user_letter, word)
-    if not guessed:
+    guessed_indices = get_indices(user_letter, word)
+
+    if not guessed_indices:
         tries_left -= 1
-        continue
-        
-    for i in guessed:
+    for i in guessed_indices:
         guess[i] = user_letter
     
+    # reset for next guess
+    user_letter = ""
+    
     if "".join(guess) == word:
-        print("You guessed the word " + word + "!")
+        print(f"You guessed the word '{word}'!")
         break
         
 if "".join(guess) != word:
